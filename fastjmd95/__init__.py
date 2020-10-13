d2cfebef91a03e9b1f818d71c522eb03754d4b39
Spencer Jones <spencerj@swotservd.ldeo.columbia.edu>
2020-10-13 09:55:26 -0400

    code refactor & rename functions

--- fastjmd95/__init__.py
+++ fastjmd95/__init__.py
@@ -1,2 +1,1 @@
-from .jmd95numba import rho, drhodt, drhods
-from .jmd95wrapper import rho_x, drhodt_x, drhods_x
+from .jmd95wrapper import rho, drhodt, drhods
--- fastjmd95/jmd95wrapper.py
+++ fastjmd95/jmd95wrapper.py
@@ -6,42 +6,37 @@ import xarray as xr
 
 import fastjmd95.jmd95numba as jmd95numba
 
+def _any_dask_array(a,b,c):
+    any_dask = any([isinstance(a,dask.array.core.Array),
+           isinstance(b,dask.array.core.Array),
+           isinstance(c,dask.array.core.Array)])
+    return any_dask
 
-def rho_x(s, t, p):
-    if any([isinstance(s,dask.array.core.Array),
-           isinstance(t,dask.array.core.Array),
-           isinstance(p,dask.array.core.Array)]):
-        rho = dsa.map_blocks(jmd95numba.rho,s,t,p)
-    elif any([[isinstance(s,xr.DataArray),
-           isinstance(t,xr.DataArray),
-           isinstance(p,xr.DataArray)]]):
-        rho = xr.apply_ufunc(jmd95numba.rho,s,t,p,dask='allowed')
-    else:
-        rho = jmd95numba.rho(s,t,p)
-    return rho
+def _any_xarray(a,b,c):
+    any_xarray = any([[isinstance(a,xr.DataArray),
+           isinstance(b,xr.DataArray),
+           isinstance(c,xr.DataArray)]])
+    return any_xarray
 
-def drhodt_x(s, t, p):
-    if any([isinstance(s,dask.array.core.Array),
-           isinstance(t,dask.array.core.Array),
-           isinstance(p,dask.array.core.Array)]):
-        drhodt = dsa.map_blocks(jmd95numba.drhodt,s,t,p)
-    elif any([[isinstance(s,xr.DataArray),
-           isinstance(t,xr.DataArray),
-           isinstance(p,xr.DataArray)]]):
-        drhodt = xr.apply_ufunc(jmd95numba.drhodt,s,t,p,dask='allowed')
-    else:
-        drhodt = jmd95numba.rho(s,t,p)
-    return drhodt
+def my_decorator(func):
+    def wrapper(*args):
+        if _any_dask_array(*args):
+            rho = dsa.map_blocks(func,*args)
+        elif _any_xarray(*args):
+            rho = xr.apply_ufunc(func,*args,dask='allowed')
+        else:
+            rho = func(*args)
+        return rho
+    return wrapper
 
-def drhods_x(s, t, p):
-    if any([isinstance(s,dask.array.core.Array),
-           isinstance(t,dask.array.core.Array),
-           isinstance(p,dask.array.core.Array)]):
-        drhods = dsa.map_blocks(jmd95numba.drhods,s,t,p)
-    elif any([[isinstance(s,xr.DataArray),
-           isinstance(t,xr.DataArray),
-           isinstance(p,xr.DataArray)]]):
-        drhods = xr.apply_ufunc(jmd95numba.drhods,s,t,p,dask='allowed')
-    else:
-        drhods = jmd95numba.rho(s,t,p)
-    return drhods
+@my_decorator
+def rho(s,t,p):
+    return jmd95numba.rho(s,t,p)
+    
+@my_decorator
+def drhodt(s,t,p):
+    return jmd95numba.drhodt(s,t,p)
+
+@my_decorator
+def drhods(s,t,p):
+    return jmd95numba.drhods(s,t,p)
