2a1480fa30548082fa1a91110741ac3df982a0ff
Spencer Jones <spencerj@swotservd.ldeo.columbia.edu>
2020-11-23 10:02:06 -0500

    Removed prints and added try at start

--- fastjmd95/jmd95wrapper.py
+++ fastjmd95/jmd95wrapper.py
@@ -1,29 +1,33 @@
 
 import fastjmd95.jmd95numba as jmd95numba
 
+try:
+    import dask.array as dsa
+except ImportError:
+    dsa = None
+
+try:
+    import xarray as xr
+except ImportError:
+    xr = None
+
 def _any_dask_array(*args):
-    try:
-        import dask.array as dsa
+    if dsa:
         return any([isinstance(a, dsa.core.Array) for a in args])
-    except ImportError:
-        print("Can't parse dask arrays if dask isn't installed")
+    else:
         return False
 
 def _any_xarray(*args):
-    try:
-        import xarray as xr
+    if xr:
         return any([isinstance(a, xr.DataArray) for a in args])
-    except ImportError:
-        print("Can't parse xarrays if xarray isn't installed")
+    else:
         return False
 
 def maybe_wrap_arrays(func):
     def wrapper(*args):
         if _any_dask_array(*args):
-            import dask.array as dsa
             rho = dsa.map_blocks(func,*args)
         elif _any_xarray(*args):
-            import xarray as xr
             rho = xr.apply_ufunc(func,*args,output_dtypes=[float],dask='parallelized')
         else:
             rho = func(*args)
